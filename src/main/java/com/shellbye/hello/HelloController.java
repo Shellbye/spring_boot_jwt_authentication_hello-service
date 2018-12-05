b5922bf0ff92120a007b60433c342279332365d1
shellbye <shellbye.com@gmail.com>
2018-12-05 14:37:02 +0800

    add rest-service to use eureka

--- src/main/java/com/shellbye/hello/HelloController.java
+++ src/main/java/com/shellbye/hello/HelloController.java
@@ -1,17 +1,40 @@
 package com.shellbye.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 public class HelloController {
 
     /**
      * https://spring.io/guides/gs/spring-boot/
-     * */
+     */
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/hello")
     public String index() {
-        return "Greetings from Spring Boot!";
+        // This is how eureka works
+        String msg = restTemplate.getForObject("http://rest-service/msg/", String.class);
+        return "Greetings from Spring Boot demo [Hello]! with msg: " + msg;
     }
 
+}
+
+@Configuration
+class RestTemplateConfig {
+
+    // Create a bean for restTemplate to call services
+    @Bean
+    @LoadBalanced        // Load balance between service instances running at different ports.
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
